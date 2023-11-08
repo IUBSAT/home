e11525bde9637105ae8fb0cd3e6bea443b8f3e62
Gourav Pullela <41987728+gpullela@users.noreply.github.com>
2023-11-08 00:51:35 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "✨ IUBSAT 🛰️",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
